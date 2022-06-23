7840a2853036586c44e25a760c2832bca736c380
yang <1762086020@qq.com>
2022-06-23 18:09:15 +0800

    忘记了

---
+++ myTest.js
@@ -0,0 +1,38 @@
+// 角度转弧度
+function angle2Radian(angle) {
+    return angle * (Math.PI / 180)
+}
+
+// 弧度转角度
+function radian2Angle(radian) {
+    return (180 / Math.PI) * radian
+}
+
+const xie = Math.sqrt(2)
+
+// 两点之间与x轴的夹角
+function angle(a, b) {
+    var dx = b.x - a.x
+    var dy = b.y - a.y
+    var dis = Math.sqrt(dx * dx + dy * dy)
+    var rota = dis > 0 ? Math.round((Math.asin(dy / dis) / Math.PI) * 180) : 0
+    // rota范围（-90，90）当b点在a点左边时，必要另处理
+    if (b.x < a.x) {
+        rota = 180 - rota
+    }
+    return rota
+}
+const org = { x: 0, y: 0 }
+console.log(angle(org, { x: -1, y: -1 }))
+
+function jdyx(angle, r, org = { x: 0, y: 0 }) {
+    const x1 = org.x + r * Math.cos((angle * Math.PI) / 180)
+
+    const y1 = org.y + r * Math.sin((angle * Math.PI) / 180)
+    return {
+        x: x1,
+        y: y1,
+    }
+}
+
+console.log(jdyx(135, Math.sqrt(2)))
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,49 @@
+// 坐标
+export interface Coordinate {
+    x: number
+    y: number
+}
+
+// 环的配置
+export interface RingConf {
+    // 开始角度
+    sAngle: number
+    // 结束角度
+    eAngle: number
+    // 环的宽度
+    ringW: number
+    // 圆心
+    org: number
+    // 半径, 环到圆心的距离
+    r: number
+    // 环的颜色
+    ringColor: string
+}
+
+// 轴标的配置项
+export interface AxisMark {
+    fontSize: number
+    fontColor: string
+}
+
+// 拖拽的 按钮样式
+export interface DragBtn {
+    // 半径
+    r: number
+    // 背景颜色
+    bgc: number
+}
+
+// 配置项
+export interface CircleSliderConf {
+    sAngle: number
+    eAngle: number
+    ringW: number
+}
+
 class CircleSlider {
-    
+    constructor() {}
+    // get spanAngle () {
+    //     return Math.abs(this.eAngle - this.sAngle)
+    // }
 }
